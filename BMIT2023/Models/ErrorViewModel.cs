4c5e1af66705abea13ec2305e95bf6287c220249
larennnn <115696452+larennnn@users.noreply.github.com>
2025-12-11 03:12:34 +0800

    Add files via upload

---
+++ BMIT2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BMIT2023.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
