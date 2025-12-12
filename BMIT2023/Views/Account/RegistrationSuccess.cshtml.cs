9679ea91f8058fb59af823e76c1b2d4e7cf9a4db
larennnn <115696452+larennnn@users.noreply.github.com>
2025-12-12 17:03:39 +0800

    Add files via upload

---
+++ BMIT2023/Views/Account/RegistrationSuccess.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BMIT2023.Views.Account
+{
+    public class RegistrationSuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
